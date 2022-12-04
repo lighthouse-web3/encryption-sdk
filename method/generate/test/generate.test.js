790a45e12a9177aad5ae6596ecdea9a52f7c2712
xlassix <46370698+xlassix@users.noreply.github.com>
2022-12-04 18:43:07 +0100

    SDK

--- method/generate/test/generate.test.js
+++
@@ -1,7 +0,0 @@
-const _package = require("..");
-
-test("Generate Key", async () => {
-  const { masterKey, keyShards } = await _package.generateKey();
-  expect(typeof masterKey).toBe("string");
-  expect(keyShards.length).toBe(5);
-}, 20000);
