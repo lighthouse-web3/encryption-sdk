c0951b32a360f35a82935fd9a8d8101b2423d2f6
xlassix <awhy14539@gmail.com>
2023-08-26 07:42:19 +0100

    transferOwnerShip

---
+++ methods/transferOwnership/index.js
@@ -0,0 +1,56 @@
+const axios = require("axios");
+
+module.exports.transferOwnership = async (
+  address,
+  cid,
+  newOwner,
+  signature,
+  resetSharedTo = true
+) => {
+  try {
+    const nodeIndexSelected = [1, 2, 3, 4, 5];
+    const nodeUrl = nodeIndexSelected.map((elem) =>
+      isDev
+        ? `${lighthouseBLSNodeDev}:900${elem}/api/transferOwnership/${elem}`
+        : `${lighthouseBLSNode}/api/transferOwnership/${elem}`
+    );
+    // send encryption key
+    const recoveredShards = await Promise.all(
+      nodeUrl.map((url) => {
+        return axios
+          .post(
+            url,
+            {
+              address,
+              cid,
+              newOwner,
+              resetSharedTo
+            },
+            {
+              headers: {
+                Authorization: "Bearer " + signature,
+              },
+            }
+          )
+          .then((res) => {
+            return res?.data?.payload;
+          });
+      })
+    );
+    return {
+      shards: recoveredShards,
+      error: null,
+    };
+  } catch (err) {
+    if (err?.response?.data?.message.includes("null")) {
+      return {
+        shards: [],
+        error: `cid not found`,
+      };
+    }
+    return {
+      shards: [],
+      error: err?.response?.data || err.message,
+    };
+  }
+};
