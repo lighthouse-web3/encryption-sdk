d2c7df960fe3337faba48fd778f6dab7be1aa8f9
aakash-taneja <aakashtaneja38@gmail.com>
2024-04-23 19:41:38 +0530

    save shard refactored

--- src/methods/saveShards/index.ts
+++ src/methods/saveShards/index.ts
@@ -2,7 +2,6 @@ import { AuthToken, KeyShard } from "../../types";
 import { API_NODE_HANDLER } from "../../util";
 import { isEqual, isCidReg } from "../../util";
 
-
 export const saveShards = async (
   address: string,
   cid: string,
@@ -13,50 +12,85 @@ export const saveShards = async (
   if (!isCidReg(cid)) {
     return {
       isSuccess: false,
-      error: "Invalid CID"
+      error: "Invalid CID",
     };
   }
   if (!Array.isArray(keyShards) || keyShards.length != 5) {
     return {
       isSuccess: false,
-      error:
-        "keyShards must be an array of 5 objects"
+      error: "keyShards must be an array of 5 objects",
+    };
+  }
+  const nodeId = [1, 2, 3, 4, 5];
+  const nodeUrl = nodeId.map((elem) => `/api/setSharedKey/${elem}`);
+  const requestData = async (url: any, index: any) => {
+    try {
+      return await API_NODE_HANDLER(
+        url,
+        "POST",
+        auth_token,
+        shareTo.length > 0
+          ? {
+              address,
+              cid,
+              payload: keyShards[index],
+              sharedTo: shareTo,
+            }
+          : {
+              address,
+              cid,
+              payload: keyShards[index],
+            }
+      );
+    } catch (error) {
+      return { error };
     }
+  };
+  const data = [];
+  for (const [index, url] of nodeUrl.entries()) {
+    const response = await requestData(url, index);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    data.push(response);
   }
-  try {
-    const nodeId = [1, 2, 3, 4, 5];
-    const nodeUrl = nodeId.map((elem) => `/api/setSharedKey/${elem}`
-    );
-    // send encryption key
-    const data = await Promise.all(
-      nodeUrl.map((url, index) => {
-        return API_NODE_HANDLER
-          (
-            url, "POST", auth_token,
-            shareTo.length > 0
-              ? {
-                address,
-                cid,
-                payload: keyShards[index],
-                sharedTo: shareTo,
-              }
-              : {
-                address,
-                cid,
-                payload: keyShards[index],
-              },
-          )
-      })
-    );
-    let temp = data.map((elem, index) => ({ ...elem, data: null }));
-    return {
-      isSuccess: isEqual(...temp) && data[0]?.message === "success",
-      error: null,
-    };
-  } catch (err: any) {
-    return {
-      isSuccess: false,
-      error:JSON.parse(err.message),
-    };
-  }
+  const temp = data.map((elem, index) => ({ ...elem, data: null }));
+  return {
+    isSuccess: isEqual(...temp) && data[0]?.message === "success",
+    error: null,
+  };
+  // try {
+  //   const nodeId = [1, 2, 3, 4, 5];
+  //   const nodeUrl = nodeId.map((elem) => `/api/setSharedKey/${elem}`
+  //   );
+  //   // send encryption key
+  //   const data = await Promise.all(
+  //     nodeUrl.map((url, index) => {
+  //       return API_NODE_HANDLER
+  //         (
+  //           url, "POST", auth_token,
+  //           shareTo.length > 0
+  //             ? {
+  //               address,
+  //               cid,
+  //               payload: keyShards[index],
+  //               sharedTo: shareTo,
+  //             }
+  //             : {
+  //               address,
+  //               cid,
+  //               payload: keyShards[index],
+  //             },
+  //         )
+  //     })
+  //   );
+  //   let temp = data.map((elem, index) => ({ ...elem, data: null }));
+  //   return {
+  //     isSuccess: isEqual(...temp) && data[0]?.message === "success",
+  //     error: null,
+  //   };
+  // } catch (err: any) {
+  //   return {
+  //     isSuccess: false,
+  //     error:JSON.parse(err.message),
+  //   };
+  // }
 };
