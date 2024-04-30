73f1dbdd1d17f94b914622855035652c46b26240
aakash-taneja <aakashtaneja38@gmail.com>
2024-04-30 20:18:50 +0530

    added try catch in saveShards

--- src/methods/saveShards/index.ts
+++ src/methods/saveShards/index.ts
@@ -21,40 +21,50 @@ export const saveShards = async (
       error: "keyShards must be an array of 5 objects",
     };
   }
-  const nodeId = [1, 2, 3, 4, 5];
-  const nodeUrl = nodeId.map((elem) => `/api/setSharedKey/${elem}`);
-  const requestData = async (url: any, index: any) => {
-    try {
-      return await API_NODE_HANDLER(
-        url,
-        "POST",
-        auth_token,
-        shareTo.length > 0
-          ? {
-              address,
-              cid,
-              payload: keyShards[index],
-              sharedTo: shareTo,
-            }
-          : {
-              address,
-              cid,
-              payload: keyShards[index],
-            }
-      );
-    } catch (error) {
-      return { error };
+  try {
+    const nodeId = [1, 2, 3, 4, 5];
+    const nodeUrl = nodeId.map((elem) => `/api/setSharedKey/${elem}`);
+    const requestData = async (url: any, index: any) => {
+      try {
+        return await API_NODE_HANDLER(
+          url,
+          "POST",
+          auth_token,
+          shareTo.length > 0
+            ? {
+                address,
+                cid,
+                payload: keyShards[index],
+                sharedTo: shareTo,
+              }
+            : {
+                address,
+                cid,
+                payload: keyShards[index],
+              }
+        );
+      } catch (error: any) {
+        return {
+          isSuccess: false,
+          error: JSON.parse(error.message),
+        };
+      }
+    };
+    const data = [];
+    for (const [index, url] of nodeUrl.entries()) {
+      const response = await requestData(url, index);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      data.push(response);
     }
-  };
-  const data = [];
-  for (const [index, url] of nodeUrl.entries()) {
-    const response = await requestData(url, index);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    data.push(response);
+    const temp = data.map((elem, index) => ({ ...elem, data: null }));
+    return {
+      isSuccess: isEqual(...temp) && data[0]?.message === "success",
+      error: null,
+    };
+  } catch (err: any) {
+    return {
+      isSuccess: false,
+      error: JSON.parse(err.message),
+    };
   }
-  const temp = data.map((elem, index) => ({ ...elem, data: null }));
-  return {
-    isSuccess: isEqual(...temp) && data[0]?.message === "success",
-    error: null,
-  };
 };
