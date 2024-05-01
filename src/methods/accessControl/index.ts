d0836e42f3ed5c251fd77edf9036d4c36181ad92
aakash-taneja <aakashtaneja38@gmail.com>
2024-05-01 13:10:30 +0530

    fix accessControl

--- src/methods/accessControl/index.ts
+++ src/methods/accessControl/index.ts
@@ -1,4 +1,11 @@
-import { AuthToken, Condition, DecryptionType, KeyShard, ChainType, LightHouseSDKResponse } from "../../types";
+import {
+  AuthToken,
+  Condition,
+  DecryptionType,
+  KeyShard,
+  ChainType,
+  LightHouseSDKResponse,
+} from "../../types";
 import config from "../../config";
 import { updateConditionSchema, accessConditionSchema } from "./validator";
 import { isEqual, isCidReg, API_NODE_HANDLER } from "../../util/index";
@@ -11,30 +18,36 @@ export const accessControl = async (
   aggregator?: string,
   chainType: ChainType = "evm",
   keyShards: KeyShard[] = [],
-  decryptionType: DecryptionType = "ADDRESS",
+  decryptionType: DecryptionType = "ADDRESS"
 ): Promise<LightHouseSDKResponse> => {
   try {
-    if (!Array.isArray(keyShards) || (keyShards.length != 5 && keyShards.length != 0)) {
+    if (
+      !Array.isArray(keyShards) ||
+      (keyShards.length != 5 && keyShards.length != 0)
+    ) {
       throw new Error("keyShards must be an array of 5 objects");
     }
     if (!isCidReg(cid)) {
       throw new Error("Invalid CID");
     }
-    const { error } = keyShards.length == 5 ? accessConditionSchema.validate({
-      address,
-      cid,
-      conditions,
-      aggregator,
-      decryptionType,
-      chainType,
-      keyShards
-    }) : updateConditionSchema.validate({
-      address,
-      cid,
-      conditions,
-      aggregator,
-      chainType,
-    });
+    const { error } =
+      keyShards.length == 5
+        ? accessConditionSchema.validate({
+            address,
+            cid,
+            conditions,
+            aggregator,
+            decryptionType,
+            chainType,
+            keyShards,
+          })
+        : updateConditionSchema.validate({
+            address,
+            cid,
+            conditions,
+            aggregator,
+            chainType,
+          });
 
     if (error) {
       throw new Error(`Condition validation error: ${error.message}`);
@@ -46,34 +59,44 @@ export const accessControl = async (
         : `/api/fileAccessConditions/${elem}`
     );
     // send encryption key
-    const data = keyShards.length == 5 ? await Promise.all(
-      nodeUrl.map((url, index) => {
-        return API_NODE_HANDLER(url, "POST", auth_token, {
-          address,
-          cid,
-          conditions,
-          aggregator,
-          decryptionType,
-          chainType,
-          payload: keyShards[index],
-        })
-      }
-      )) : await Promise.all(
-        nodeUrl.map((url) => {
-          return API_NODE_HANDLER(
-            url, "PUT", auth_token,
-            {
+    const requestData = async (url: any, index: any) => {
+      try {
+        return keyShards.length === 5
+          ? await API_NODE_HANDLER(url, "POST", auth_token, {
+              address,
+              cid,
+              conditions,
+              aggregator,
+              decryptionType,
+              chainType,
+              payload: keyShards[index],
+            })
+          : await API_NODE_HANDLER(url, "PUT", auth_token, {
               address,
               cid,
               conditions,
               aggregator,
               chainType,
-            }
-          )
-        })
-      );
+            });
+      } catch (error: any) {
+        return {
+          isSuccess: false,
+          error: JSON.parse(error.message),
+        };
+      }
+    };
+    const data = [];
+    for (const [index, url] of nodeUrl.entries()) {
+      const response = await requestData(url, index);
+      if (response.error)
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second before retrying
+
+      data.push(response);
+    }
     return {
-      isSuccess: isEqual(...data.map(e => e.message)) && data[0]?.message === "success",
+      isSuccess:
+        isEqual(...data.map((e) => e.message)) &&
+        data[0]?.message === "success",
       error: null,
     };
   } catch (err: any) {
